530010bf4d88ef3576313445f1d2a2a2655a7816
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-23 17:56:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,21 +3,21 @@ class Dog
 
   attr_accessor :name
 
-@@all = []
+  @@all = []
 
-def initialize(name)
-  @name = name
-  @@all << self
-end
-
-def self.all
-  @@all.each do |dog|
-    puts dog.name
+  def initialize(name)
+    @name = name
+    @@all << self
   end
-end
 
-def self.clear_all
-  @@all.clear
-end
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
 
 end
